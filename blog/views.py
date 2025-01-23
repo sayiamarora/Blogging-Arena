f406d1d142c0584fd8373e697d93c870a68a9a25
Sayiam Arora <sayiam0127@gmail.com>
2025-01-24 03:08:25 +0530

    Login page

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 def test(request):
-  return render(request,'blog/base.html')
+  return render(request,'blog/login.html')
 # Create your views here.
