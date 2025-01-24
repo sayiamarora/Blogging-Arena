2d59cc64432fea252ce9ed3c0a81d92ec47f5ce5
Sayiam Arora <sayiam0127@gmail.com>
2025-01-24 13:47:36 +0530

    Signup page

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.test),
+    # path('', views.login),
+    path('login/',views.login),
+    path('',views.signup),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 def test(request):
-  return render(request,'blog/login.html')
+  return render(request,'blog/base.html')
+
+def login(request):
+  return render(request, 'blog/login.html')
+
+def signup(request):
+  return render(request,'blog/signup.html')      
 # Create your views here.
